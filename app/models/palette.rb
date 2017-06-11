de8fc85ff10996d0c76b5639d824ac12bb1a5b76
nriser <riserika@gmail.com>
2017-06-11 14:53:08 -0400

    Establish ERD relationship between User & Palette
    For Palette model, set relationship as 'belongs_to :user'.
    For User model, set relationship as 'has_many :palettes'.

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -1,2 +1,8 @@
 class Palette < ApplicationRecord
+  belongs_to :user
+
+  validates :color1, presence: true
+  validates :color2, presence: true
+  validates :color3, presence: true
+  validates :color4, presence: true
 end
